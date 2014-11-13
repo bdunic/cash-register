d5abd84a9e9175f780261b6e3c1bd750f21849db
bdunic <bdunic@nebrwesleyan.edu>
2014-11-13 10:03:44 -0500

    cash register

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 "use strict"; // This line chooses a JavaScript dialect, one that helps both jsLint (used in OrionHub) and browsers catch errors.
 /*jslint browser: true*/ // This line tells jsLint that the code will run in a browser.
 
+<button type="button"><img src="cake.png" alt="cake"></button>
