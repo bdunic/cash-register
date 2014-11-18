be452f17bc56e2be4ede90975b9902259e274899
bdunic <bdunic@nebrwesleyan.edu>
2014-11-18 09:35:59 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 "use strict"; // This line chooses a JavaScript dialect, one that helps both jsLint (used in OrionHub) and browsers catch errors.
 /*jslint browser: true*/ // This line tells jsLint that the code will run in a browser.
 
-<button type="button"><img src="cake.png" alt="cake"></button>
+var prices = {
+	'hamburger': 1.99,
+	'cheeseburger': 2.29,
+	'corndog': 1.75,
+	'fries': 1.19,
+	'cookie': .75,
+	'icecream': .99,
+	'drink': 1.25,
+	'coffee': 1.99,
+	'sundae': 2.89,
+}
