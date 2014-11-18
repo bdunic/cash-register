9ee65d7a8b600cf99f2ef241e0fd5d222bf6c7fa
bdunic <bdunic@nebrwesleyan.edu>
2014-11-18 09:38:42 -0600

    Update index.js

--- index.js
+++ index.js
@@ -12,3 +12,46 @@ var prices = {
 	'coffee': 1.99,
 	'sundae': 2.89,
 }
+var runningsubtotal, runningtax, runningtotal;
+runningsubtotal=0
+runningtax=0
+runningtotal=0
+function Register(item){
+var subtotal, total, tax;
+subtotal = prices[item];
+tax=prices[item] * .07;
+total= subtotal + tax;
+runningsubtotal=runningsubtotal+subtotal;
+runningtax=runningtax+tax;
+runningtotal=runningtotal+total
+}
+var i=1
+function confirm() {
+runningtotal=0;
+runningsubtotal=0;
+runningtax=0
+i=i+1
+}
+function clear() {
+runningtotal=0;
+runningsubtotal=0;
+runningtax=0;
+}
+function hamburger(){
+Register('hamburger');
+}
+function cheeseburger(){
+Register('cheeseburger');
+}
+function corndog(){
+Register('crondog');
+}
+function drink(){
+Register('drink');
+}
+function sundae(){
+Register('sundae');
+}
+function coffee(){
+Register('coffee');
+}
